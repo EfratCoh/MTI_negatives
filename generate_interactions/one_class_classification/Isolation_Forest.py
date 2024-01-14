5a92f6b0e8c3db2df0f16cf5ad48406a38bdfe11
EfratCoh <102877528+EfratCoh@users.noreply.github.com>
2024-01-14 19:04:02 +0200

    Update Isolation_Forest.py

--- generate_interactions/one_class_classification/Isolation_Forest.py
+++ generate_interactions/one_class_classification/Isolation_Forest.py
@@ -29,7 +29,7 @@ from utilsfile import read_csv, to_csv
 import Classifier.FeatureReader as FeatureReader
 from Classifier.FeatureReader import get_reader
 from Classifier.ClfLogger import logger
-from consts.global_consts import  ROOT_PATH, DATA_PATH_INTERACTIONS, NEGATIVE_DATA_PATH, MERGE_DATA, DATA_PATH_INTERACTIONS
+from consts.global_consts import  ROOT_PATH, DATA_PATH_INTERACTIONS, NEGATIVE_DATA_PATH, MERGE_DATA, DATA_PATH_INTERACTIONS,one_class_params_path_yml
 from sklearn.ensemble import IsolationForest
 
 class NoModelFound(Exception):
@@ -71,7 +71,6 @@ class ClassifierWithGridSearch(object):
 
         # creat the specific clf and load the parameters of the clf according to the ymal file.
         clf = self.clf_dict[clf_name]
-        print(clf)
         parameters = conf['parameters']
 
         # this step find to optimize prams
@@ -93,18 +92,7 @@ class ClassifierWithGridSearch(object):
         results = pd.DataFrame(grid_obj.cv_results_)
         results.to_csv(output_file, index=False)
 
-        # def fit_best_clf(self, clf_name):
-        #     clf = self.clf_dict[clf_name]
-        #     print(clf)
-        #     fit_params = {}
-        #     if clf_name == "xgbs":
-        #         fit_params = {"eval_set": [(self.X, self.y)],
-        #                       "early_stopping_rounds": 50}
-        #
-        #     clf.fit(self.X, self.y, **fit_params)
-        #     clf.save_model("model_sklearn.json")
-        #
-        #     return clf
+    
 
     def fit(self, yaml_path,number_iteration):
         with open(yaml_path, 'r') as stream:
@@ -140,54 +128,13 @@ def self_fit(feature_mode, yaml_file, first_self, last_self, name_method, dir_me
 
 def build_classifiers_isolation_forest(number_iteration):
     number_iteration = str(number_iteration)
-    yaml_file = "/sise/home/efrco/efrco-master/Classifier/yaml/one_class_params.yml"
+    yaml_file = one_class_params_path_yml
     FeatureReader.reader_selection_parameter = "without_hot_encoding"
     self_fit("without_hot_encoding", yaml_file, 1, 2, name_method="one_class_svm", dir_method="models_isolation_forest", number_iteration=number_iteration)
 
     print("END main_primary")
 
-# build_classifiers_isolation_forest(number_iteration=0)
-
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
-
-def isolation():
-    FeatureReader.reader_selection_parameter = "without_hot_encoding"
-
-    train = "/sise/home/efrco/efrco-master/data/train/underSampling/0/tarBase_human_negative_features_train_underSampling_method_0.csv"
-    test = "/sise/home/efrco/efrco-master/data/test/one_class_svm/0/tarBase_human_negative_test_one_class.csv"
-    # test = "/sise/home/efrco/efrco-master/data/test/underSampling/0/tarBase_human_negative_features_test_underSampling_method_0.csv"
-    feature_reader = get_reader()
-    X_train, y_train = feature_reader.file_reader(train)
-    y_train = pd.DataFrame(y_train, columns=['Label'])
-    train = pd.concat([X_train, y_train], axis=1)
-    train = train.drop(train[train['Label'] == 0].sample(frac=0.8, random_state=40).index)
-    y = train['Label']
-    # y = [0 if i==1 else 1 for i in y ]
-    print(train['Label'].value_counts(normalize=True))
-    train.drop(columns=["Label"], inplace=True)
-
-    X_test, y_test = feature_reader.file_reader(test)
-    # print(y_test.value_counts(normalize=True))
 
 
-    # y_test = pd.DataFrame(y_test)
-    # test = pd.concat([X_test, y_test], axis=1)
-    # test = test.drop(test[test['Label'] == 0].sample(frac=0.8, random_state=40).index)
-    # y = test['Label']
-    # test.drop(columns=["Label"], inplace=True)
-
-    if_model = RandomForestClassifier(n_estimators=100, random_state=0).fit(train,y)
-    # Predict the anomalies
-    if_prediction = if_model.predict(X_test)
-    test_score = accuracy_score(y_test, if_prediction)
-    print("TEST results" , test_score)
-    auc = roc_auc_score(y_test, if_prediction)
-    print("TEST results auc" , auc)
 
 
-    # Change the anomalies' values to make it consistent with the true values
-    # if_prediction = [0 if i==-1 else 1 for i in if_prediction]
-    # Check the model performance
-    print(classification_report(y_test, if_prediction))
-
-# isolation()
