e435999d3912be24e9b67d52f04602fa3b9136dd
EfratCoh <102877528+EfratCoh@users.noreply.github.com>
2024-01-14 19:10:47 +0200

    Update duplex_step.py

--- pipeline_steps/duplex_step.py
+++ pipeline_steps/duplex_step.py
@@ -49,25 +49,4 @@ def duplex(method: str, fin: str, fout: str):
     to_csv(result, Path(fout))
 
 
-# def duplex(method: str, fin: str, fout: str):
-#
-#     duplex_cls: Duplex = DUPLEX_DICT[method]
-#     logger.info(f"{method} do_duplex to {fin}")
-#     in_df: DataFrame = read_csv(Path(fin))
-#     seq_cols = ['miRNA sequence', 'full_mrna', 'site_old']
-#     in_df[seq_cols] = in_df[seq_cols].replace(to_replace='T', value='U', regex=True)
-#     duplex_df = in_df.apply(func=get_wrapper(
-#         do_duplex, "miRNA sequence", "site_old", cls=duplex_cls), axis=1)
-#     in_df.drop(columns=['site_old'], inplace=True)
-#
-#     result = pd.merge(left=in_df, right=duplex_df, left_index=True, right_index=True, how='left')
-#     result = result[result['duplex_valid'] == True]
-#     result["duplex_method"] = method
-#     to_csv(result, Path(fout))
 
-
-if __name__ == '__main__':
-    # cli()
-    fin = ROOT_PATH / "generate_interactions/new_1.csv"
-    fout =ROOT_PATH / "generate_interactions/duplex.csv"
-    duplex('ViennaDuplex', fin, fout)
